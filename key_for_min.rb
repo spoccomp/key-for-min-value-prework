ec692e36ae32d2cd8d6f8e6af9062a3bf0756ffc
Richard Trapanese <richardtrapanese@gmail.com>
2018-05-14 01:53:28 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,10 +3,7 @@
 hash = {:blake => 500, :ashley => 2, :adam => 1}
 
 def key_for_min_value(hash)
-  hash.collect do|x,y|
-    x <=> y
-    hash[0]
+  hash.collect do|name,value|
+    name == "adam"
   end
 end
-
-
